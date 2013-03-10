3a10e98d84d8d57d2401a319dc57a354526cd057
techtangents <dylan@techtangents.com>
2013-03-10 21:14:30 +1000

    main now displays data from dummy source

--- src/elm/Techtangents/Fewtile/Main.elm
+++ src/elm/Techtangents/Fewtile/Main.elm
@@ -3,6 +3,7 @@ module Techtangents.Fewtile.Main where
 import Techtangents.Fewtile.Tile
 import Techtangents.Fewtile.Alien.Cycler
 import Techtangents.Fewtile.Alien.NonEmpty
+import Techtangents.Fewtile.Sources.DummySource
 
 title = constant (JavaScript.castStringToJSString "Fewtile")
 foreign export jsevent "elm_title"
@@ -14,6 +15,13 @@ main =
     [filled red (rect 100 100 (120, 120))]
 -}
 
+source = dummySource
+
 list = NonEmpty 1 [4, 9, 12]
 
-main = asText <~ (Automaton.run (cycler list) (every second))
+--main = asText <~ (Automaton.run (cycler list) (every second))
+
+tileSummary tiles =
+  concat (List.intersperse "," (map (\(Tile t) -> t.text) tiles))
+
+main = asText <~ (tileSummary <~ source)
--- src/elm/Techtangents/Fewtile/Sources/DummySource.elm
+++ src/elm/Techtangents/Fewtile/Sources/DummySource.elm
@@ -1,5 +1,23 @@
 module Techtangents.Fewtile.Sources.DummySource where
 
 import Techtangents.Fewtile.Source
+import Techtangents.Fewtile.Alien.Cycler
+import Techtangents.Fewtile.Tile
+import Techtangents.Fewtile.Alien.NonEmpty
 
---dummySource =
+s1t1 = Tile { id = "1", text = "hello", weight = 3, color = red }
+s1t2 = Tile { id = "2", text = "chook", weight = 3, color = green }
+
+s2t1 = Tile { id = "1", text = "hello2", weight = 3, color = cyan }
+s2t2 = Tile { id = "2", text = "chook2", weight = 3, color = magenta }
+
+state1 = [s1t1, s1t2]
+state2 = [s2t1, s2t2]
+
+-- dummyTiles : NonEmpty [Tile]
+dummyTiles =
+ NonEmpty state1 [state2]
+
+-- dummySource : Signal [Tile]
+dummySource =
+  Automaton.run (cycler dummyTiles) (every $ second * 2)
