fca0a4b59d046263b166dea56cef2e5c92e9621a
techtangents <dylan@techtangents.com>
2014-07-16 20:30:54 +1000

    layout

--- src/Techtangents/Fewtile/Layout/Layout.elm
+++ src/Techtangents/Fewtile/Layout/Layout.elm
@@ -13,20 +13,26 @@ aspectRatio : Float
 aspectRatio = 
   7 / 3
 
+type Alloc = {start: Int, size: Int}
+
+{-| Yeah, I'm getting weird type errors with 'max' -}
+natto : Int -> Int
+natto x = if x >= 0 then x else 0
 
 {-|
   Allocate a length of space into slices, given the weights of the slices.
 
   Sizes are quantized to integers by rounding.
-  In calculating each slice's size, its weight is taken as a percentage of the remaining weight to be allocated.
+  In calculating each slice's size, its weight is taken as a percentage of the remaining 
+  weight to be allocated.
   This percentage of the remaining space is calculated then rounded.
   I suspect that the difference in slice size between the rounded versions and if you just allocated 
   to floats is less than 1 for each slice.
 
   The returned List of allocations is parallel to the input list.
 -}
-allocate : Int -> [Int] -> [{start:Int, size: Int}]
-allocate totalSize weights = 
+linearAllocate : Int -> [Int] -> [Int]
+linearAllocate totalSize weights = 
   let 
     totalWeight = List.sum weights
     initialState = { remWeight = totalWeight
@@ -37,17 +43,36 @@ allocate totalSize weights =
       List.foldr
         (\weight {remWeight, remSize, acc} -> 
           let
-            size = if remWeight == 0 
-                      then 0 
-                      else round <| (toFloat weight) / (toFloat remWeight) * (toFloat remSize)
-            newRemSize = remSize - size
+            s = if (remWeight == 0)
+                   then 0 
+                   else round <| (toFloat weight) / (toFloat remWeight) * (toFloat remSize)
+            newRemSize = natto (remSize - s)
           in
             { remWeight = remWeight - weight
             , remSize   = newRemSize
-            , acc       = {start = newRemSize, size = size} :: acc
+            , acc       = s :: acc
             }
         )
         initialState
         weights
   in r.acc
   
+
+linearPosition : Int -> [Int] -> [Alloc]
+linearPosition padding als = 
+  let l = List.foldl 
+            (\size (acc, newStart) -> 
+              ({start=newStart,size=size} :: acc, newStart+size+padding)
+            ) 
+            ([], padding)
+            als
+  in  reverse (fst l)
+
+linearLayout : Int -> Int -> [Int] -> [Alloc]
+linearLayout padding totalSize weights =
+  let len = List.length weights
+      nonBorderSpace = (2 + len) * padding
+  in  if (nonBorderSpace <= 2 * len) 
+        then []
+        else linearPosition padding (linearAllocate nonBorderSpace weights)
+
--- src/Techtangents/Fewtile/Test/Tests/LayoutTest.elm
+++ src/Techtangents/Fewtile/Test/Tests/LayoutTest.elm
@@ -6,22 +6,12 @@ import ElmTest.Runner.Element (..)
 
 import Techtangents.Fewtile.Layout.Layout (..)
 
+
 tests : Test
 tests = 
   suite "Layout" 
-  [ defaultTest (assertEqual 
-                  [{start=0, size=1}] 
-                  (allocate 1 [1])
-                )
-  , defaultTest (assertEqual 
-                  [{start=0, size=2}] 
-                  (allocate 2 [1])
-                )
-  , defaultTest (assertEqual 
-                  [{start=0, size=10}, {start=10, size=20}]
-                  (allocate 30 [1, 2])
-                )
-  , defaultTest (assertEqual 
-                  [{start=0, size=100}, {start=100, size=200}, {start=300, size=200}] 
-                  (allocate 500 [1, 2, 2]))
+  [ defaultTest (assertEqual [1] (linearAllocate 1 [1]))
+  , defaultTest (assertEqual [2] (linearAllocate 2 [1]))
+  , defaultTest (assertEqual [10, 20] (linearAllocate 30 [1, 2]))
+  , defaultTest (assertEqual [100, 200, 200] (linearAllocate 500 [1, 2, 2]))
   ]
